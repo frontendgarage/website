1b948532ce9c19eb0fc344f8fdc79f22e9d39843
Kemil Beltre <20250204+kemilbeltre@users.noreply.github.com>
2023-12-08 19:36:42 +0100

    feat: change theme switcher component (#9)

--- src/core/layouts/base-layout/header/index.tsx
+++ src/core/layouts/base-layout/header/index.tsx
@@ -9,8 +9,8 @@ import classnames from 'classnames'
 import { Dialog, Divider, Popover } from '@core/components'
 import { Urls } from '@core/constants'
 
-import ThemeSwitch from './theme-switch'
 import styles from './styles.module.css'
+import { ThemeSelect, ThemeToggle } from './theme'
 
 export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -68,17 +68,25 @@ export default function Header() {
             Blog
           </Link>
         </Popover.Group>
-        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
+          <ThemeToggle panelClassName="mt-8" />
+
           <Link
             target="_blank"
             rel="noopener"
             href={Urls.GITHUB}
             className={styles.link}
           >
-            GitHub
+            <span className="sr-only">Frontend Garage on GitHub</span>
+            <svg
+              viewBox="0 0 16 16"
+              className="w-5 h-5"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z" />
+            </svg>
           </Link>
-
-          <ThemeSwitch className="ml-2" />
         </div>
       </nav>
       <Dialog
@@ -158,7 +166,7 @@ export default function Header() {
                   GitHub
                 </Link>
 
-                <ThemeSwitch />
+                <ThemeSelect />
               </div>
             </div>
           </div>
--- src/core/layouts/base-layout/header/theme-switch/types.ts
+++
@@ -1,3 +0,0 @@
-import type { ComponentProps } from 'react'
-
-export interface ThemeSwitchProperties extends ComponentProps<'select'> {}
