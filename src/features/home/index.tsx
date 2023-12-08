cd643d2f2851e9f54a02d13fd35bb6e0edbeb754
Kemil Beltre <kemilbeltre@gmail.com>
2023-12-08 23:22:29 +0100

    feat: wip add home page

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,3 +1,16 @@
+import styles from './styles.module.css'
+
 export default function Home() {
-  return 'Main content'
+  return (
+    <div className="w-full h-96 flex items-center justify-center m-auto px-8">
+      <div>
+        <h3 className={styles.heading}>
+          Welcome to <span className="text-purple-500">Frontend Garage</span>
+        </h3>
+        <h3 className={styles.heading}>
+          a place for <span className="text-blue-500">Learn</span>
+        </h3>
+      </div>
+    </div>
+  )
 }
