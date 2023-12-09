9cf082060cf8471316b18b0c4bb9ac18eff3dec9
Kemil Beltre <kemilbeltre@gmail.com>
2023-12-09 14:51:51 +0100

    feat: add main content

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -2,13 +2,14 @@ import styles from './styles.module.css'
 
 export default function Home() {
   return (
-    <div className="w-full h-96 flex items-center justify-center m-auto px-8">
+    <div className={styles.container}>
       <div>
         <h3 className={styles.heading}>
           Welcome to <span className="text-purple-500">Frontend Garage</span>
         </h3>
         <h3 className={styles.heading}>
-          a place for <span className="text-blue-500">Learn</span>
+          a place for <span className={'text-blue-500'}>Learn </span>
+          and <span className={'text-green-500'}>Growth</span>.
         </h3>
       </div>
     </div>
