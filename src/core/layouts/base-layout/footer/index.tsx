58fcb3030c0d0c86d551b72c162730d8a8880b65
Kemil Beltre <kemilbeltre@gmail.com>
2023-12-09 16:12:07 +0100

    refactor: separate styles from component

--- src/core/layouts/base-layout/footer/index.tsx
+++ src/core/layouts/base-layout/footer/index.tsx
@@ -1,9 +1,11 @@
+import styles from './styles.module.css'
+
 export default function Footer() {
   const year = new Date().getFullYear()
   return (
-    <footer className="absolute bottom-8 mx-0 w-full grid place-items-center">
+    <footer className={styles.container}>
       <h2 className="sr-only">Footer</h2>
-      <p className="text-xs">© {year} | All Rights reserved</p>
+      <p className={styles.copyright}>© {year} | All Rights reserved</p>
     </footer>
   )
 }
