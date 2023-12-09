11faa2c450d2fd6ecce76c7e08798cb924427fc5
Kemil Beltre <20250204+kemilbeltre@users.noreply.github.com>
2023-12-09 14:55:02 +0100

    feat: add home page main content (#10)
    * feat: wip add home page
    
    * feat: animate logo
    
    * feat: add main content

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,3 +1,17 @@
+import styles from './styles.module.css'
+
 export default function Home() {
-  return 'Main content'
+  return (
+    <div className={styles.container}>
+      <div>
+        <h3 className={styles.heading}>
+          Welcome to <span className="text-purple-500">Frontend Garage</span>
+        </h3>
+        <h3 className={styles.heading}>
+          a place for <span className={'text-blue-500'}>Learn </span>
+          and <span className={'text-green-500'}>Growth</span>.
+        </h3>
+      </div>
+    </div>
+  )
 }
