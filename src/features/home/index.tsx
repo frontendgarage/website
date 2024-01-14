e49209a58ce69418da973f02b7dc49f67d6e3441
Kemil Beltre <kemilbeltre@gmail.com>
2024-01-14 23:51:32 +0100

    feat: add embed substack subscribe component

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div>
+      <div className="mb-12">
         <h3 className={styles.heading}>
           Welcome to <span className="text-purple-500">Frontend Garage</span>
         </h3>
@@ -11,6 +11,12 @@ export default function Home() {
           a place for <span className={'text-blue-500'}>Learn </span>
           and <span className={'text-green-500'}>Growth</span>.
         </h3>
+      </div>
+      <div className={styles.subscribe}>
+        <p className="mb-2">
+          Join the community who read my free weekly Newsletter.
+        </p>
+        <iframe src="https://frontendgarage.substack.com/embed"></iframe>
       </div>
     </div>
   )
