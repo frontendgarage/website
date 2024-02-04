073aa34b1e5eedeb5070bc31d03c2526f25c16b9
Kemil Beltre <kemilbeltre@gmail.com>
2024-02-04 20:46:51 +0100

    refactor: remove css module from home component

--- src/features/home/index.tsx
+++ src/features/home/index.tsx
@@ -1,18 +1,16 @@
-import styles from './styles.module.css'
-
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="w-full h-96 flex flex-col items-center justify-center pt-24 px-8">
       <div className="mb-12">
-        <h3 className={styles.heading}>
+        <h3 className="text-2xl font-semibold lg:text-6xl">
           Welcome to <span className="text-purple-500">Frontend Garage</span>
         </h3>
-        <h3 className={styles.heading}>
+        <h3 className="text-2xl font-semibold lg:text-6xl">
           a place for <span className={'text-blue-500'}>Learn </span>
           and <span className={'text-green-500'}>Growth</span>.
         </h3>
       </div>
-      <div className={styles.subscribe}>
+      <div className="flex flex-col">
         <p className="mb-2">
           Join the community who read my free weekly Newsletter.
         </p>
